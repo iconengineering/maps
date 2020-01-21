3863ef271d0b7a160675ae027f8db9772df7ddfc
jdeischer <jdeischer@iconeng.com>
2020-01-20 19:06:48 -0700

    Reset template. XS and City FP added

--- fort-collins/west-vine/westvine.js
+++ fort-collins/west-vine/westvine.js
@@ -1,412 +1,120 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiaWNvbmVuZyIsImEiOiJjaXBwc2V1ZnMwNGY3ZmptMzQ3ZmJ0ZXE1In0.mo_STWygoqFqRI-od05qFg';
-if (!mapboxgl.supported()) {
-  alert('Your browser does not support Mapbox GL');
-} else {
-  var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/iconeng/cjahqpuz797612sqajznqxkyw',
-    zoom: 14.5,
-    center: [-105.11, 40.59],
-  });
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/iconeng/civjrd2la004z2immqynhr4fd',
+  zoom: 13.75,
+  center: [-105.1122, 40.5920],
+  hash: true,
+  preserveDrawingBuffer: true
+});
 
-  map.on('style.load', function() {
+var layerList = document.getElementById('menu');
+var inputs = layerList.getElementsByTagName('input');
 
-    map.addSource('cityFP',{
-      type:'geojson',
-      "data": 'cityfp.geojson'
-    })
-
-  map.addLayer({
-    'id':'cityFP',
-    'type':'line',
-    'source':'cityFP',
-    'paint':{
-      'line-width':2
-    }
-  });
-
-    map.addSource('wvb-contours', {
-      type: 'vector',
-      url: 'mapbox://iconeng.7b288ff0'
-    });
-    //WVB 1 FT CONTOURS
-    map.addLayer({
-      'id': 'wvb-1ftContours',
-      'type': 'line',
-      'source': 'wvb-contours',
-      'source-layer': 'wvb_contours',
-      'filter': ['all', ['==', 'Index', 0]],
-      'layout': {
-        'line-join': 'round',
-        'visibility': 'none',
-        'line-cap': 'round'
-      },
-      'paint': {
-        'line-width': {
-          "stops": [
-            [15, 0],
-            [17, .5],
-            [19, 1]
-          ]
-        },
-        'line-color': '#bd925a'
-      }
-    }, 'road_label');
-    //WVB 5 FT CONTOURS
-    map.addLayer({
-      'id': 'wvb-5ftContours',
-      'type': 'line',
-      'source': 'wvb-contours',
-      'source-layer': 'wvb_contours',
-      'filter': ['all', ['>=', 'Index', 5],
-        ['<=', 'Index', 10]
-      ],
-      'layout': {
-        'line-join': 'round',
-        'visibility': 'none',
-        'line-cap': 'round'
-      },
-      'paint': {
-        'line-width': {
-          "stops": [
-            [15, 1],
-            [17, 1.75],
-            [19, 2.5]
-          ]
-        },
-        'line-color': '#bd925a'
-      }
-    }, 'road_label');
-    //WVB 5 FT LABELS
-    map.addLayer({
-      'id': 'wvb-5ftLabels',
-      'type': 'symbol',
-      'source': 'wvb-contours',
-      'source-layer': 'wvb_contours',
-      'filter': ['all', ['>=', 'Index', 5],
-        ['<=', 'Index', 10]
-      ],
-      'layout': {
-        'symbol-placement': 'line',
-        'visibility': 'none',
-        'text-field': '{CONTOUR}',
-        'text-size': {
-          "stops": [
-            [15, 12],
-            [17, 14],
-            [19, 16]
-          ]
-        }
-      },
-      'paint': {
-        'text-color': '#bd925a',
-        'text-halo-color': '#F8F4F0',
-        'text-halo-width': 2,
-        'text-halo-blur': 0.5,
-      }
-    });
-    map.addSource('wvb-basins', {
-      type: 'vector',
-      url: 'mapbox://iconeng.5n8brw3p'
-    });
-
-    //WVB BASINS
-    map.addLayer({
-      'id': 'wvb-basins',
-      'type': 'fill',
-      'source': 'wvb-basins',
-      'source-layer': 'wvb_basins',
-      'layerGroup': 1,
-      'interactive': true,
-      'layout': {
-        'visibility': 'none'
-      },
-      'paint': {
-        'fill-color': '#1407dc',
-        'fill-opacity': 0.05
-      }
-    }, 'road_label');
-    // WVB BASIN BORDER
-    map.addLayer({
-      "id": "wvb-basins-borders",
-      "type": "line",
-      "source": "wvb-basins",
-      'source-layer': 'wvb_basins',
-      "layout": {
-        'visibility': 'none'
-      },
-      "paint": {
-        "line-color": "#1407dc",
-        "line-width": 2
-      }
-    }, 'road_label');
-
-    map.addSource('canals', {
-      type: 'vector',
-      url: 'mapbox://iconeng.8bkept6e'
-    });
-    //CANAL
-    map.addLayer({
-      'id': 'canals',
-      'type': 'line',
-      'source': 'canals',
-      'source-layer': 'wvb_canals',
-      'layout': {
-        'line-join': 'round',
-        'visibility': 'visible',
-        'line-cap': 'round'
-      },
-      'paint': {
-        'line-width': {
-          "stops": [
-            [15, 1],
-            [17, 2],
-            [19, 4]
-          ]
-        },
-        'line-color': '#33ffff'
-      }
-    });
-    //CANAL LABELS
-    map.addLayer({
-      'id': 'canalLabels',
-      'type': 'symbol',
-      'source': 'canals',
-      'source-layer': 'wvb_canals',
-      'layout': {
-        'symbol-placement': 'line',
-        'symbol-spacing': 100,
-        'visibility': 'visible',
-        'text-field': '{Name}',
-        'text-size': {
-          "stops": [
-            [15, 12],
-            [17, 14],
-            [19, 16]
-          ]
-        },
-        "text-padding": 100,
-      },
-      'paint': {
-        'text-color': '#000',
-        'text-halo-color': 'rgba(75,255,255,0.9)',
-        'text-halo-width': 2,
-        'text-halo-blur': 1
-      }
-    });
-
-    map.addSource('wvb-xs', {
-      type: 'geojson',
-      "data": 'data/xs.geojson'
-    });
-    //WEST VINE XS
-    map.addLayer({
-      'id': 'wvb-xs',
-      'type': 'line',
-      'source': 'wvb-xs',
-      'paint': {
-        'line-width': 2,
-        'line-opacity': 1,
-        'line-color': 'rgba(0,0,0,1)'
-      },
-      'layout': {
-        'visibility': 'visible'
-      }
-    });
-
-    //WEST VINE XS LABELS
-    map.addLayer({
-      'id': 'wvb-xsLabels',
-      'type': 'symbol',
-      'source': 'wvb-xs',
-      'layout': {
-        'symbol-placement': 'line',
-        'symbol-spacing': 100,
-        'text-field': '{XSName}',
-        'text-size': {
-          "stops": [
-            [15, 12],
-            [17, 14],
-            [19, 16]
-          ]
-        },
-        "text-padding": 100,
-      },
-      'paint': {
-        'text-color': '#000',
-        'text-halo-color': '#ffffff',
-        'text-halo-width': 2,
-        'text-halo-blur': 1
-      }
-    });
-
-    map.addSource('wvb-fp-100yr', {
-      type: 'geojson',
-      "data": 'data/fp-100yr-polygons.geojson'
-    });
-
-    //West Vine 100 yr FP
-    map.addLayer({
-      'id': 'wvb-fp-100yr',
-      'type': 'line',
-      'source': 'wvb-fp-100yr',
-      'paint': {
-        'line-width': 1,
-        'line-opacity': 0.6,
-        'line-color': 'rgba(0,230,255,1)'
-      },
-      'layout': {
-        'visibility': 'visible'
-      }
-    });
-
-    //100-YR FLOODPLAIN FILL
-    map.addLayer({
-      'id': 'wvb-fp-100yr-fill',
-      'type': 'fill',
-      'source': 'wvb-fp-100yr',
-      'layerGroup': 1,
-      'interactive': true,
-      'layout': {
-        'visibility': 'visible'
-      },
-      'paint': {
-        'fill-color': 'rgba(0,230,255,1)',
-        'fill-opacity': 0.3
-      }
-    }, 'road_label');
-
-    map.addSource('wvb-fp-sf', {
-      type: 'geojson',
-      "data": "data/fp-shallow-flooding-polygons.geojson"
-    });
-
-    //Add shallow flooding
-    map.addLayer({
-      'id': 'wvb-fp-sf',
-      'type': 'line',
-      'source': 'wvb-fp-sf',
-      'paint': {
-        'line-width': 1,
-        'line-opacity': 0.6,
-        'line-color': 'rgb(255,128,0)'
-      },
-      'layout': {
-        'visibility': 'visible'
-      }
-    });
-
-    //SHALLOW FLOODING FLOODPLAIN FILL
-    map.addLayer({
-      'id': 'wvb-fp-sf-fill',
-      'type': 'fill',
-      'source': 'wvb-fp-sf',
-      'layerGroup': 1,
-      'interactive': true,
-      'layout': {
-        'visibility': 'visible'
-      },
-      'paint': {
-        'fill-color': 'rgba(255,128,0,1)',
-        'fill-opacity': 0.3
-      }
-    }, 'road_label');
-
-
-    map.addSource('wvb-stream', {
-      type: 'geojson',
-      "data": 'data/river.geojson'
-    });
-
-    //West Vine Stream centerline
-    map.addLayer({
-      'id': 'wvb-stream',
-      'type': 'line',
-      'source': 'wvb-stream',
-      'paint': {
-        'line-width': 1.5,
-        'line-opacity': 1,
-        'line-color': 'rgba(0,77,168,1)'
-      },
-      'layout': {
-        'visibility': 'visible'
-      }
-    });
-
-    //West Vine Stream Centerline LABEL
-    map.addLayer({
-      'id': 'wvb-streamLabels',
-      'type': 'symbol',
-      'source': 'wvb-stream',
-      'layout': {
-        'symbol-placement': 'line',
-        'symbol-spacing': 100,
-        'text-field': '{RiverName}' + ' - ' + '{ReachName}',
-        'text-size': {
-          "stops": [
-            [15, 12],
-            [17, 14],
-            [19, 16]
-          ]
-        },
-        "text-padding": 100,
-      },
-      'paint': {
-        'text-color': 'rgba(0,77,168,1)',
-        'text-halo-color': '#ffffff',
-        'text-halo-width': 2,
-        'text-halo-blur': 1
-      }
-    });
-
-    map.addSource('wvb-roadway', {
-      type: 'geojson',
-      "data": 'data/roadwaycrossings.geojson'
-    });
-    //West Vine Roadway Crossings
-    map.addLayer({
-      'id': 'wvb-roadway',
-      'type': 'line',
-      'source': 'wvb-roadway',
-      'paint': {
-        'line-width': 2,
-        'line-opacity': 1,
-        'line-color': 'rgba(128,128,128,1)'
-      },
-      'layout': {
-        'visibility': 'visible'
-      }
-    });
-
-  }); //end style load
-
-  // When a click event occurs on a feature in the states layer, open a popup at the
-  // location of the click, with description HTML from its properties.
-  map.on('click', function(e) {
-    var features = map.queryRenderedFeatures(e.point, {
-      layers: ['wvb-xs']
-    });
-    if (!features.length) {
-      return;
-    }
-
-    var feature = features[0];
-
-    var popup = new mapboxgl.Popup()
-      .setLngLat(e.lngLat)
-      .setHTML(feature.properties.RiverName + ' ' + feature.properties.ReachName + '<br>' + 'XS: ' + feature.properties.XSName + '<br>' + 'WSEL: ' + feature.properties.XSWSElev)
-      .addTo(map);
-  });
-
-  // Use the same approach as above to indicate that the symbols are clickable
-  // by changing the cursor style to 'pointer'.
-  map.on('mousemove', function(e) {
-    var features = map.queryRenderedFeatures(e.point, {
-      layers: ['wvb-xs']
-    });
-    map.getCanvas().style.cursor = (features.length) ? 'pointer' : '';
-  });
-
-  map.addControl(new mapboxgl.NavigationControl(), 'top-right');
-
+function switchLayer(layer) {
+  var layerId = layer.target.value;
+  map.setStyle('mapbox://styles/iconeng/' + layerId);
+  $('.layer-off').prop('checked', false);
+  $('.layer-on').prop('checked', true);
 }
+
+for (var i = 0; i < inputs.length; i++) {
+  inputs[i].onclick = switchLayer;
+}
+
+$(document).ready(function() {
+  $("#clear").click(function() {
+    var checkBoxes = $("input[type=checkbox]");
+    checkBoxes.prop("checked", false);
+    map.setPaintProperty('basinOutlines', 'line-opacity', 0);
+    map.setPaintProperty('draingeways', 'line-opacity', 0)
+    map.setPaintProperty('flowDepth', 'visibility', 'none');
+    map.setPaintProperty('1ftContours', 'line-opacity', 0);
+    map.setPaintProperty('5ftContours', 'line-opacity', 0);
+    map.setPaintProperty('5ftLabels', 'text-opacity', 0);
+    map.setPaintProperty('parcels', 'line-opacity', 0);
+    map.setPaintProperty('rasboudnary', 'line-opacity', 0);
+  });
+});
+
+map.on('style.load', function(e) {
+
+  map.addSource('wvb-xs', {
+    type: 'geojson',
+    "data": 'data/xs.geojson'
+  });
+
+  map.addSource('flowDepth', {
+    type: 'vector',
+    url: 'mapbox://iconeng.7ir8lsql'
+  });
+
+  map.addSource('cityFP',{
+    type:'geojson',
+    "data": 'data/cityfp.geojson'
+  })
+
+map.addLayer({
+  'id':'cityFP',
+  'type':'fill',
+  'filter':['all',['==',"FP_NAME","WEST VINE"],['==',"FLOOD_TYPE","100 YEAR"]],
+  'source':'cityFP',
+  'paint':{
+    'fill-opacity':0.5
+  }
+});
+
+//WEST VINE XS
+map.addLayer({
+  'id': 'wvb-xs',
+  'type': 'line',
+  'source': 'wvb-xs',
+  'paint': {
+    'line-width': 2,
+    'line-opacity': 1,
+    'line-color': 'rgba(0,0,0,1)'
+  },
+  'layout': {
+    'visibility': 'visible'
+  }
+});
+
+
+}); //end style load
+
+// When a click event occurs near a marker icon, open a popup at the location of
+
+// the feature, with description HTML from its properties.
+
+// When a click event occurs near a marker icon, open a popup at the location of
+// the feature, with description HTML from its properties.
+
+//Cross Section Labels
+map.on('click', function(e) {
+  var features = map.queryRenderedFeatures(e.point, {
+    layers: ['wvb-xs']
+  });
+  if (!features.length) {
+    return;
+  }
+
+  var feature = features[0];
+
+  var popup = new mapboxgl.Popup()
+    .setLngLat(e.lngLat)
+    .setHTML(feature.properties.RiverName + ' ' + feature.properties.ReachName + '<br>' + 'XS: ' + feature.properties.XSName + '<br>' + 'WSEL:' + feature.properties.XSWSElev)
+    .addTo(map);
+});
+
+// Use the same approach as above to indicate that the symbols are clickable
+// by changing the cursor style to 'pointer'.
+map.on('mousemove', function(e) {
+  var features = map.queryRenderedFeatures(e.point, {
+    layers: ['wvb-xs']
+  });
+  map.getCanvas().style.cursor = (features.length) ? 'pointer' : '';
+});
+
+
+
+map.addControl(new mapboxgl.NavigationControl(), 'top-right');
---
+++ fort-collins/west-vine/willow-creek-tribs.js
@@ -0,0 +1,296 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiaWNvbmVuZyIsImEiOiJjaXBwc2V1ZnMwNGY3ZmptMzQ3ZmJ0ZXE1In0.mo_STWygoqFqRI-od05qFg';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/iconeng/civjrd2la004z2immqynhr4fd',
+  zoom: 13,
+  center: [-104.8969, 39.5666],
+  hash: true,
+  preserveDrawingBuffer: true
+});
+
+var layerList = document.getElementById('menu');
+var inputs = layerList.getElementsByTagName('input');
+
+function switchLayer(layer) {
+  var layerId = layer.target.value;
+  map.setStyle('mapbox://styles/iconeng/' + layerId);
+  $('.layer-off').prop('checked', false);
+  $('.layer-on').prop('checked', true);
+}
+
+for (var i = 0; i < inputs.length; i++) {
+  inputs[i].onclick = switchLayer;
+}
+
+$(document).ready(function() {
+  $("#clear").click(function() {
+    var checkBoxes = $("input[type=checkbox]");
+    checkBoxes.prop("checked", false);
+    map.setPaintProperty('basinOutlines', 'line-opacity', 0);
+    map.setPaintProperty('draingeways', 'line-opacity', 0)
+    map.setPaintProperty('flowDepth', 'visibility', 'none');
+    map.setPaintProperty('1ftContours', 'line-opacity', 0);
+    map.setPaintProperty('5ftContours', 'line-opacity', 0);
+    map.setPaintProperty('5ftLabels', 'text-opacity', 0);
+    map.setPaintProperty('parcels', 'line-opacity', 0);
+    map.setPaintProperty('rasboudnary', 'line-opacity', 0);
+  });
+});
+
+map.on('style.load', function(e) {
+
+  map.addSource('basinOutlines', {
+    type: 'geojson',
+    "data": 'basinOutlines.geojson'
+  });
+  map.addSource('contours', {
+    type: 'vector',
+    url: 'mapbox://iconeng.d80vvltu'
+  });
+  map.addSource('drainageways', {
+    type: 'vector',
+    url: 'mapbox://iconeng.6xshx73b'
+  });
+  map.addSource('flowDepth', {
+    type: 'vector',
+    url: 'mapbox://iconeng.7ir8lsql'
+  });
+  map.addSource('parcels', {
+    type: 'geojson',
+    "data": 'parcels.geojson'
+  });
+  map.addSource('rasboundary', {
+    type: 'geojson',
+    "data": 'rasboundary.geojson'
+  });
+  map.addSource('cityFP',{
+    type:'geojson',
+    "data": 'cityfp.geojson'
+  })
+
+map.addLayer({
+  'id':'cityFP',
+  'type':'fill',
+  'filter':['all',['==',"FP_NAME","WEST VINE"],['==',"FLOOD_TYPE","100 YEAR"]],
+  'source':'cityFP',
+  'paint':{
+    'fill-opacity':0.5
+  }
+});
+
+
+  //Add Basin Outlines
+  map.addLayer({
+    'id': 'basinOutlines',
+    'type': 'line',
+    'source': 'basinOutlines',
+    'paint': {
+      'line-width': 1,
+      'line-opacity': 1,
+      'line-color': 'rgba(0,0,0,1)',
+      'line-dasharray': [8, 4]
+    }
+  });
+
+  //Add 5 ft Contours
+  map.addLayer({
+    'id': '5ftContours',
+    'type': 'line',
+    'source': 'contours',
+    'source-layer': 'contours_clipped-c5t1bg',
+    'filter': ['all', ['>', 'Index', 1]],
+    'layout': {
+      'line-join': 'round',
+      'line-cap': 'round'
+    },
+    'paint': {
+      'line-width': {
+        "stops": [
+          [15, 1],
+          [17, 1.75],
+          [19, 2.5]
+        ]
+      },
+      'line-opacity': 0,
+      'line-color': '#bd925a'
+    }
+  }, 'road-label-small');
+
+  //  Add 1ft contours
+  map.addLayer({
+    'id': '1ftContours',
+    'type': 'line',
+    'source': 'contours',
+    'source-layer': 'contours_clipped-c5t1bg',
+    'layout': {
+      'line-join': 'round',
+      'line-cap': 'round'
+    },
+    'paint': {
+      'line-width': {
+        "stops": [
+          [15, 0],
+          [17, .5],
+          [19, 1]
+        ]
+      },
+      'line-opacity': 0,
+      'line-color': '#bd925a'
+    }
+  }, 'road-label-small');
+
+  // 5 ft. Contour Labels
+  map.addLayer({
+    'id': '5ftLabels',
+    'type': 'symbol',
+    'source': 'contours',
+    'source-layer': 'contours_clipped-c5t1bg',
+    'filter': ['all', ['>', 'Index', 1]],
+    'layout': {
+      'symbol-placement': 'line',
+      'text-field': '{CONTOUR}',
+      'text-font': ['Roboto Light Italic', 'Open Sans Light', 'Arial Unicode MS Regular'],
+      'text-size': {
+        "stops": [
+          [15, 9],
+          [17, 11],
+          [19, 13]
+        ]
+      }
+    },
+    'paint': {
+      'text-color': '#bd925a',
+      'text-halo-color': '#F8F4F0',
+      'text-halo-width': 2,
+      'text-halo-blur': 0.5,
+      'text-opacity': 0
+    }
+  }, 'road-label-small');
+
+  //Add Drainageways
+  map.addLayer({
+    'id': 'drainageways',
+    'type': 'line',
+    'source': 'drainageways',
+    'source-layer': 'drainageways-cbyi1o',
+    'paint': {
+      'line-width': 2,
+      'line-opacity': 1,
+      'line-color': 'rgba(75,255,255,0.9)',
+    }
+  });
+
+  //Add Drainageway Labels
+  map.addLayer({
+    'id': 'dwayLabels',
+    'type': 'symbol',
+    'source': 'drainageways',
+    'source-layer': 'drainageways-cbyi1o',
+    'layout': {
+      'symbol-placement': 'line',
+      'symbol-spacing': 100,
+      'text-field': '{str_name}',
+      'text-size': {
+        "stops": [
+          [15, 12],
+          [17, 14],
+          [19, 16]
+        ]
+      },
+      "text-padding": 100,
+    },
+    'paint': {
+      'text-color': '#000',
+      'text-halo-color': 'rgba(75,255,255,0.9)',
+      'text-halo-width': 2,
+      'text-halo-blur': 1
+    }
+  });
+
+  //Add Flow depth
+  map.addLayer({
+    'id': 'flowDepth',
+    'type': 'fill',
+    'source': 'flowDepth',
+    'source-layer': 'wct_depth_merged-6uujq7',
+    'filter': ['>=', 'VALUE', 0.08],
+    'paint': {
+      'fill-color': {
+        property: 'VALUE',
+        type: 'interval',
+        stops: [
+          [.25, 'rgb(252,244,182)'],
+          [.5, 'rgb(245,194,152)'],
+          [1, 'rgb(227,147,138)'],
+          [1.5, 'rgb(199,101,134)'],
+          [2, 'rgb(161,59,139)'],
+          [3, 'rgb(109,23,143)'],
+          [4, 'rgb(14,9,135)']
+        ]
+      },
+      'fill-opacity': 0.8
+    },
+    'layout': {
+      'visibility': 'none'
+    }
+  }, 'road-label-small');
+
+  //Add parcels to layer
+  map.addLayer({
+    'id': 'parcels',
+    'type': 'line',
+    'source': 'parcels',
+    'paint': {
+      'line-color': '#dc0714',
+      'line-width': 1,
+      'line-opacity': 0
+    },
+  });
+
+  // Add RAS Boundary to mapbox
+  map.addLayer({
+      'id': 'rasboundary',
+      'type': 'line',
+      'source': 'rasboundary',
+      'paint': {
+        'line-color': '#008000',
+        'line-width': 3,
+        'line-opacity': 0
+      },
+  });
+}); //end style load
+
+// When a click event occurs near a marker icon, open a popup at the location of
+
+// the feature, with description HTML from its properties.
+
+map.on('click', function(e) {
+
+  var features = map.queryRenderedFeatures(e.point, {
+    layers: ['flowDepth']
+  });
+  if (!features.length) {
+    return;
+  }
+  var feature = features[0];
+  if (feature.layer.id == 'flowDepth') {
+    var popup = new mapboxgl.Popup()
+      .setLngLat(e.lngLat)
+      .setHTML('<span>Depth: </span>' + Math.round(feature.properties.VALUE * 100) / 100 + ' ft')
+      .addTo(map);
+
+  } else {
+    return;
+  }
+});
+
+// Change cursor over clickable features
+map.on('mousemove', function(e) {
+  var features = map.queryRenderedFeatures(e.point, {
+    layers: ['flowDepth']
+  });
+  map.getCanvas().style.cursor = (features.length) ? 'pointer' : '';
+});
+
+
+map.addControl(new mapboxgl.NavigationControl(), 'top-right');
