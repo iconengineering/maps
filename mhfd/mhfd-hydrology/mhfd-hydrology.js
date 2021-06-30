30363ab61f5ff839091feac5e08021f8e127c2f5
Jeremy Deischer <41126368+jeremydeischer@users.noreply.github.com>
2021-06-30 16:37:08 -0600

    Enable flow values

--- mhfd/mhfd-hydrology/mhfd-hydrology.js
+++ mhfd/mhfd-hydrology/mhfd-hydrology.js
@@ -532,278 +532,124 @@ map.on('style.load',function(e){
 });
 
 
+map.on('click', function (e) {
+  var features = map.queryRenderedFeatures(e.point, { layers: ['conduits','junctions'] });
+  if (!features.length) {
+      return;
+  }
 
+  var feature = features[0];
+  var id = feature.layer.id
 
+    if (id == 'conduits' || id == 'junctions'){
 
-// map.on('click', function (e) {
-//   var features = map.queryRenderedFeatures(e.point, { layers: ['conduits','junctions','conduitComps','junctionComps'] });
-//   if (!features.length) {
-//       return;
-//   }
-//
-//   var feature = features[0];
-//   var id = feature.layer.id
-//
-//     if (id == 'conduits' || id == 'junctions'){
-//
-//       var data = [{name: "2yr", value: feature.properties.Result_2},
-//                   {name: "10yr", value: feature.properties.Result_10},
-//                   {name: "100yr", value: feature.properties.Result_100}];
-//
-//     var	margin = {top: 10, right: 40, bottom: 30, left: 50},
-//         width = 240 - margin.left - margin.right,
-//         height = 240 - margin.top - margin.bottom;
-//
-//         var x = d3.scaleBand()
-//             .rangeRound([0, width])
-//             .paddingInner(0.1);
-//
-//         var y = d3.scaleLinear()
-//             .range([height, 0]);
-//
-//         var z = d3.scaleOrdinal()
-//             .range(['#089099','#00718b','#045275']);
-//
-//       var div = window.document.createElement('div');
-//       if (id == 'conduits' && feature.properties.Shape1 == 'IRREGULAR') {
-//         div.innerHTML = '<div class="row"><b>Conduit ' + feature.properties.Name + '</b><br />Irregular</div>';
-//       } else if (id == 'conduits' && feature.properties.Shape1 == 'CIRCULAR') {
-//         div.innerHTML = '<div class="row"><b>Conduit ' + feature.properties.Name + '</b><br />' + feature.properties.Geom1 + 'ft Circular</div>';
-//       } else if (id == 'conduits' && feature.properties.Shape1 == 'RECT_CLOSED') {
-//         div.innerHTML = '<div class="row"><b>Conduit ' + feature.properties.Name + '</b><br />' + feature.properties.Geom1 + 'ft x ' + feature.properties.Geom2 + 'ft Box</div>';
-//       } else {
-//         div.innerHTML = '<div class="row"><b>Junction ' + feature.properties.Name + '</b></div>';
-//       };
-//
-//       var	svg = d3.select(div)
-//         .append("svg")
-//           .attr("class", "xs")
-//           .attr("width", width + margin.left + margin.right)
-//           .attr("height", height + margin.top + margin.bottom)
-//         .append("g")
-//           .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-//
-//           d3.select(".xs").selectAll(".bar").remove()
-//
-//           x.domain(data.map(function(d) { return d.name; }));
-//           y.domain([0, d3.max(data, function(d) {
-//             if (d.value <= 100) {return 100}
-//             else if (d.value <= 500) {return 500}
-//             else if (d.value <= 1000) {return 1000}
-//             else 	{ return 1500 }
-//             ;})
-//           ]);
-//
-//           svg.selectAll(".bar")
-//               .data(data)
-//             .enter().append("rect")
-//               .attr("x", function(d) { return x(d.name); })
-//               .attr("width", x.bandwidth())
-//               .attr("y", function(d) { return height; })
-//               .attr("height", 0)
-//               .attr("fill", function(d) { return z(d.name); })
-//               .transition()
-//               .delay(function (d, i) { return i*100; })
-//               .attr("height", function(d) { return height - y(d.value); })
-//               .attr("y", function(d) { return y(d.value); });
-//
-//           svg.selectAll("text.bar")
-//               .data(data)
-//             .enter().append("text")
-//               .attr("class", "bar")
-//               .attr("text-anchor", "middle")
-//               .attr("x", function(d) { return x(d.name) + 23; })
-//               .attr("y", function(d) {
-//                 if (height - y(d.value) < 20) { return y(d.value) - 5; }
-//                 else { return y(d.value) + 12; }
-//                 ;})
-//               .attr("fill", function(d) {
-//                 if (height - y(d.value) < 20) { return '#000' }
-//                 else { return '#fff' }
-//                 ;})
-//               .attr('font-size', '10px')
-//               .attr('font-weight', 'bold')
-//               .text(function(d) { return Math.round(d.value); });
-//
-//           // Add the X Axis
-//           svg.append("g")
-//               .attr("transform", "translate(0," + height + ")")
-//               .call(d3.axisBottom(x).tickSizeOuter(0));
-//
-//           // Add the Y Axis
-//           svg.append("g")
-//               .call(d3.axisLeft(y).ticks(4))
-//             .append("text")
-//               .attr("transform", "rotate(-90)")
-//               .attr("y", 3)
-//               .attr("dy", ".71em")
-//               .attr("font-size", 8)
-//               .attr("fill", "#000")
-//               .style("text-anchor", "end")
-//               .text("cfs");
-//
-//         function type(d) {
-//           d.value = +d.value;
-//           return d;
-//         }
-//
-//       var d3popup = new mapboxgl.Popup()
-//           .setLngLat(e.lngLat)
-//           .setDOMContent(div)
-//           .addTo(map);
-//     } else if (id == 'conduitComps' || id == 'junctionComps'){
-//       var data = [{name: "2yr", value: feature.properties.Result_2, value2: feature.properties.ACE05_Q2},
-//                   {name: "10yr", value: feature.properties.Result_10, value2: feature.properties.ACE05_Q10},
-//                   {name: "100yr", value: feature.properties.Result_100, value2: feature.properties.ACE05_Q100}];
-//
-//     var	margin = {top: 10, right: 40, bottom: 30, left: 50},
-//         width = 240 - margin.left - margin.right,
-//         height = 240 - margin.top - margin.bottom;
-//
-//         var x = d3.scaleBand()
-//             .rangeRound([0, width])
-//             .paddingInner(0.1);
-//
-//         var y = d3.scaleLinear()
-//             .range([height, 0]);
-//
-//         var z0 = d3.scaleOrdinal()
-//             .range(['#089099','#00718b','#045275']);
-//
-//         var z1 = d3.scaleOrdinal()
-//             .range(['#dd686c','#ca5268','#b13f64']);
-//
-//
-//
-//       var div = window.document.createElement('div');
-//       if (id == 'conduitComps' && feature.properties.Shape1 == 'IRREGULAR') {
-//         div.innerHTML = '<div class="row"><b>Conduit ' + feature.properties.Name + '</b><br />Irregular<br /><small><span style="font-weight: bold;color: #ca5268;">2003 ANDERSON Study</span> | <span style="font-weight: bold;color: #00718b;">2017 ICON Study</span></small></div>';
-//       } else if (id == 'conduitComps' && feature.properties.Shape1 == 'CIRCULAR') {
-//         div.innerHTML = '<div class="row"><b>Conduit ' + feature.properties.Name + '</b><br />' + feature.properties.Geom1 + 'ft Circular<br /><small><span style="font-weight: bold;color: #ca5268;">2003 ANDERSON Study</span> | <span style="font-weight: bold;color: #00718b;">2017 ICON Study</span></small></div>';
-//       } else if (id == 'conduitComps' && feature.properties.Shape1 == 'RECT_CLOSED') {
-//         div.innerHTML = '<div class="row"><b>Conduit ' + feature.properties.Name + '</b><br />' + feature.properties.Geom1 + 'ft x ' + feature.properties.Geom2 + 'ft Box<br /><small><span style="font-weight: bold;color: #ca5268;">2003 ANDERSON Study</span> | <span style="font-weight: bold;color: #00718b;">2017 ICON Study</span></small></div>';
-//       }  else {
-//         div.innerHTML = '<div class="row"><b>Junction ' + feature.properties.Name + '</b><br /><small><span style="font-weight: bold;color: #ca5268;">2003 ANDERSON Study</span> | <span style="font-weight: bold;color: #00718b;">2017 ICON Study</span></small></div>';
-//       };
-//
-//       var	svg = d3.select(div)
-//         .append("svg")
-//           .attr("class", "xs")
-//           .attr("width", width + margin.left + margin.right)
-//           .attr("height", height + margin.top + margin.bottom)
-//         .append("g")
-//           .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-//
-//           d3.select(".xs").selectAll(".bar").remove()
-//
-//           x.domain(data.map(function(d) { return d.name; }));
-//           y.domain([0, d3.max(data, function(d) {
-//             if (d.value <= 100 && d.value2 <= 100) {return 100}
-//             else if (d.value <= 500 && d.value2 <= 500) {return 500}
-//             else if (d.value <= 1000 && d.value2 <= 1000) {return 1000}
-//             else 	{ return 1500 }
-//             ;})
-//           ]);
-//
-//           svg.selectAll(".bar")
-//               .data(data)
-//             .enter().append("rect")
-//               .attr("x", function(d) { return x(d.name) + 23; })
-//               .attr("width", function(d) { return x.bandwidth() / 2 } )
-//               .attr("y", function(d) { return height; })
-//               .attr("height", 0)
-//               .attr("fill", function(d) { return z0(d.name); })
-//               .transition()
-//               .delay(function (d, i) { return i*100; })
-//               .attr("height", function(d) { return height - y(d.value); })
-//               .attr("y", function(d) { return y(d.value); });
-//
-//           svg.selectAll(".bar")
-//               .data(data)
-//             .enter().append("rect")
-//               .attr("x", function(d) { return x(d.name); })
-//               .attr("width", function(d) { return x.bandwidth() / 2 } )
-//               .attr("y", function(d) { return height; })
-//               .attr("height", 0)
-//               .attr("fill", function(d) { return z1(d.name); })
-//               .transition()
-//               .delay(function (d, i) { return i*100; })
-//               .attr("height", function(d) { return height - y(d.value2); })
-//               .attr("y", function(d) { return y(d.value2); });
-//
-//           svg.selectAll("text.bar")
-//               .data(data)
-//             .enter().append("text")
-//               .attr("class", "bar")
-//               .attr("text-anchor", function(d) {
-//                 if (height - y(d.value) < 40) { return "start" }
-//                 else { return "end" }
-//                 ;})
-//               .attr("transform", "rotate(-90)")
-//               .attr("y", function(d) { return x(d.name) + 38; })
-//               .attr("x", function(d) {
-//                 if (height - y(d.value) < 40) { return -y(d.value) + 2; }
-//                 else { return -y(d.value) - 4; }
-//                 ;})
-//               .attr("fill", function(d) {
-//                 if (height - y(d.value) < 40) { return '#000' }
-//                 else { return '#fff' }
-//                 ;})
-//               .attr('font-size', '10px')
-//               .attr('font-weight', 'bold')
-//               .text(function(d) { return Math.round(d.value); });
-//
-//           svg.selectAll("text.bar2")
-//               .data(data)
-//             .enter().append("text")
-//               .attr("class", "bar2")
-//               .attr("text-anchor", function(d) {
-//                 if (height - y(d.value2) < 40) { return "start" }
-//                 else { return "end" }
-//                 ;})
-//               .attr("transform", "rotate(-90)")
-//               .attr("y", function(d) { return x(d.name) + 15; })
-//               .attr("x", function(d) {
-//                 if (height - y(d.value2) < 40) { return -y(d.value2) + 2; }
-//                 else { return -y(d.value2) - 4; }
-//                 ;})
-//               .attr("fill", function(d) {
-//                 if (height - y(d.value2) < 40) { return '#000' }
-//                 else { return '#fff' }
-//                 ;})
-//               .attr('font-size', '10px')
-//               .attr('font-weight', 'bold')
-//               .text(function(d) { return Math.round(d.value2); });
-//
-//
-//           // Add the X Axis
-//           svg.append("g")
-//               .attr("transform", "translate(0," + height + ")")
-//               .call(d3.axisBottom(x).tickSizeOuter(0));
-//
-//           // Add the Y Axis
-//           svg.append("g")
-//               .call(d3.axisLeft(y).ticks(4))
-//             .append("text")
-//               .attr("transform", "rotate(-90)")
-//               .attr("y", 3)
-//               .attr("dy", ".71em")
-//               .attr("font-size", 8)
-//               .attr("fill", "#000")
-//               .style("text-anchor", "end")
-//               .text("cfs");
-//
-//         function type(d) {
-//           d.value = +d.value;
-//           return d;
-//         }
-//
-//       var d3popup = new mapboxgl.Popup()
-//           .setLngLat(e.lngLat)
-//           .setDOMContent(div)
-//           .addTo(map);
-//         } else { return; }
-//     });
+      var data = [{name: "2-yr", value: feature.properties.Q_Ex_002},
+                  {name: "10-yr", value: feature.properties.Q_Ex_010},
+                  {name: "100-yr", value: feature.properties.Q_Ex_100}];
+
+    var	margin = {top: 10, right: 40, bottom: 30, left: 50},
+        width = 240 - margin.left - margin.right,
+        height = 240 - margin.top - margin.bottom;
+
+        var x = d3.scaleBand()
+            .rangeRound([0, width])
+            .paddingInner(0.1);
+
+        var y = d3.scaleLinear()
+            .range([height, 0]);
+
+        var z = d3.scaleOrdinal()
+            .range(['#089099','#00718b','#045275']);
+
+      var div = window.document.createElement('div');
+      if (id == 'conduits' && feature.properties.Shape1 == 'IRREGULAR') {
+        div.innerHTML = '<div class="row"><b>Conduit ' + feature.properties.NAME + '</b><br />Irregular</div>';
+      } else if (id == 'conduits' && feature.properties.Shape1 == 'CIRCULAR') {
+        div.innerHTML = '<div class="row"><b>Conduit ' + feature.properties.NAME + '</b><br />' + feature.properties.Geom1 + 'ft Circular</div>';
+      } else if (id == 'conduits' && feature.properties.Shape1 == 'RECT_CLOSED') {
+        div.innerHTML = '<div class="row"><b>Conduit ' + feature.properties.NAME + '</b><br />' + feature.properties.Geom1 + 'ft x ' + feature.properties.Geom2 + 'ft Box</div>';
+      } else {
+        div.innerHTML = '<div class="row"><b>Junction ' + feature.properties.NAME + '</b></div>';
+      };
+
+      var	svg = d3.select(div)
+        .append("svg")
+          .attr("class", "xs")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+          d3.select(".xs").selectAll(".bar").remove()
+
+          x.domain(data.map(function(d) { return d.name; }));
+          y.domain([0, d3.max(data, function(d) {
+            if (d.value <= 100) {return 100}
+            else if (d.value <= 500) {return 500}
+            else if (d.value <= 1000) {return 1000}
+            else 	{ return 1500 }
+            ;})
+          ]);
+
+          svg.selectAll(".bar")
+              .data(data)
+            .enter().append("rect")
+              .attr("x", function(d) { return x(d.name); })
+              .attr("width", x.bandwidth())
+              .attr("y", function(d) { return height; })
+              .attr("height", 0)
+              .attr("fill", function(d) { return z(d.name); })
+              .transition()
+              .delay(function (d, i) { return i*100; })
+              .attr("height", function(d) { return height - y(d.value); })
+              .attr("y", function(d) { return y(d.value); });
+
+          svg.selectAll("text.bar")
+              .data(data)
+            .enter().append("text")
+              .attr("class", "bar")
+              .attr("text-anchor", "middle")
+              .attr("x", function(d) { return x(d.name) + 23; })
+              .attr("y", function(d) {
+                if (height - y(d.value) < 20) { return y(d.value) - 5; }
+                else { return y(d.value) + 12; }
+                ;})
+              .attr("fill", function(d) {
+                if (height - y(d.value) < 20) { return '#000' }
+                else { return '#fff' }
+                ;})
+              .attr('font-size', '10px')
+              .attr('font-weight', 'bold')
+              .text(function(d) { return Math.round(d.value); });
+
+          // Add the X Axis
+          svg.append("g")
+              .attr("transform", "translate(0," + height + ")")
+              .call(d3.axisBottom(x).tickSizeOuter(0));
+
+          // Add the Y Axis
+          svg.append("g")
+              .call(d3.axisLeft(y).ticks(4))
+            .append("text")
+              .attr("transform", "rotate(-90)")
+              .attr("y", 3)
+              .attr("dy", ".71em")
+              .attr("font-size", 8)
+              .attr("fill", "#000")
+              .style("text-anchor", "end")
+              .text("cfs");
+
+        function type(d) {
+          d.value = +d.value;
+          return d;
+        }
+
+      var d3popup = new mapboxgl.Popup()
+          .setLngLat(e.lngLat)
+          .setDOMContent(div)
+          .addTo(map);
+    } else { return; }
+    });
 
 map.on('mousemove', function (e) {
     var features = map.queryRenderedFeatures(e.point, { layers: ['conduits','junctions'] });
